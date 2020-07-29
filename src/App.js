9b732ef6d4677e9cf432bb4612066b1116381fbc
GrizzlecallerTryJS <dylebedev.coding@gmail.com>
2020-07-29 19:41:40 +0300

    07 done

--- src/App.js
+++ src/App.js
@@ -3,7 +3,35 @@ import React from 'react';
 import './App.css';
 
 const App = () => {
-  return <div className='App'>Test</div>;
+  return (
+    <div className='App'>
+      <Header />
+      <Techno />
+    </div>
+  );
+};
+
+const Header = () => {
+  return (
+    <div className='App'>
+      <p>Home</p>
+      <p>News</p>
+      <p>Messages</p>
+    </div>
+  );
+};
+
+const Techno = () => {
+  return (
+    <div>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+      </ul>
+    </div>
+  );
 };
 
 export default App;
