4039a8b30275f77ca586b7f1ca19d8fa938c54e9
GrizzlecallerTryJS <dylebedev.coding@gmail.com>
2020-08-01 20:11:31 +0300

    21 done

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,7 +2,21 @@ import React from 'react';
 import styles from './Dialogs.module.css';
 
 const Dialogs = (props) => {
-  return <div>Dialogs</div>;
+  return (
+    <div className={styles.dialogs_main}>
+      <div className={styles.dialogs_items}>
+        <div className={styles.dialog}>QQQ</div>
+        <div className={styles.dialog}>WWW</div>
+        <div className={styles.dialog}>EEE</div>
+        <div className={styles.dialog}>RRR</div>
+      </div>
+      <div className={styles.dialogs_messages}>
+        <div className={styles.message}>111</div>
+        <div className={styles.message}>222</div>
+        <div className={styles.message}>333</div>
+      </div>
+    </div>
+  );
 };
 
 export default Dialogs;
