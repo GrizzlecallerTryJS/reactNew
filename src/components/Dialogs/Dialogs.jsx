84c16571872e1c528c6fb781022b079921046f7d
GrizzlecallerTryJS <dylebedev.coding@gmail.com>
2020-08-04 20:07:00 +0300

    24 done

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -16,19 +16,32 @@ const Message = (props) => {
   return <div className={styles.message}>{props.messageText}</div>;
 };
 
+let dialogsData = [
+  { id: 1, name: 'Aaron' },
+  { id: 2, name: 'Bernard' },
+  { id: 3, name: 'Christopher' },
+  { id: 4, name: 'Dylan' },
+];
+
+let messagesData = [
+  { id: 1, text: 'Sint duis nisi pariatur duis irure quis labore ut eiusmod in quis.' },
+  { id: 2, text: 'Do est occaecat elit elit in voluptate exercitation quis exercitation magna ea ullamco.' },
+  { id: 3, text: 'Laborum dolor anim Lorem ullamco veniam ad ea.' },
+];
+
 const Dialogs = (props) => {
   return (
     <div className={styles.dialogs_main}>
       <div className={styles.dialogs_items}>
-        <DialogItem name='Aaron' id='1' />
-        <DialogItem name='Bernard' id='2' />
-        <DialogItem name='Christopher' id='3' />
-        <DialogItem name='Dylan' id='4' />
+        <DialogItem id={dialogsData.name[0]} name={dialogsData.id[0]} />
+        <DialogItem id={dialogsData.name[1]} name={dialogsData.id[1]} />
+        <DialogItem id={dialogsData.name[2]} name={dialogsData.id[2]} />
+        <DialogItem id={dialogsData.name[3]} name={dialogsData.id[3]} />
       </div>
       <div className={styles.dialogs_messages}>
-        <Message messageText='Sint duis nisi pariatur duis irure quis labore ut eiusmod in quis.' />
-        <Message messageText='Do est occaecat elit elit in voluptate exercitation quis exercitation magna ea ullamco.' />
-        <Message messageText='Laborum dolor anim Lorem ullamco veniam ad ea.' />
+        <Message id={messagesData.id[0]} messageText={messagesData.text[0]} />
+        <Message id={messagesData.id[0]} messageText={messagesData.text[1]} />
+        <Message id={messagesData.id[0]} messageText={messagesData.text[2]} />
       </div>
     </div>
   );
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,6 +3,16 @@ import styles from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = () => {
+  let postData = [
+    { id: 1, message: 'BEEP', likeCount: 15 },
+    { id: 2, message: 'BOOP', likeCount: 20 },
+    { id: 3, message: 'BOOP', likeCount: 20 },
+    { id: 4, message: 'I', likeCount: 20 },
+    { id: 5, message: 'AM', likeCount: 20 },
+    { id: 6, message: 'A', likeCount: 20 },
+    { id: 7, message: 'ROBOT', likeCount: 20 },
+  ];
+
   return (
     <div>
       <div>
@@ -21,8 +31,13 @@ const MyPosts = () => {
         </div>
       </div>
       <div className={styles.posts}>
-        <Post message='Hi' likeCount={'15'} />
-        <Post message='Boop' likeCount={'20'} />
+        <Post id={postData[0].id} message={postData[0].message} likeCount={postData[0].likeCount} />
+        <Post id={postData[1].id} message={postData[1].message} likeCount={postData[1].likeCount} />
+        <Post id={postData[2].id} message={postData[2].message} likeCount={postData[2].likeCount} />
+        <Post id={postData[3].id} message={postData[3].message} likeCount={postData[3].likeCount} />
+        <Post id={postData[4].id} message={postData[4].message} likeCount={postData[4].likeCount} />
+        <Post id={postData[5].id} message={postData[5].message} likeCount={postData[5].likeCount} />
+        <Post id={postData[6].id} message={postData[6].message} likeCount={postData[6].likeCount} />
       </div>
     </div>
   );
