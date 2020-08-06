dd4242dc0283dd76a319ac629307b5d957c01917
GrizzlecallerTryJS <dylebedev.coding@gmail.com>
2020-08-06 15:13:33 +0300

    30_done

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Dialogs from './components/Dialogs/Dialogs';
 import News from './components/News/News';
 import Music from './components/Music/Music';
 import Settings from './components/Settings/Settings';
-import { Route, BrowserRouter } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 const App = (props) => {
   const dialogsCaller = () => {
@@ -18,26 +18,24 @@ const App = (props) => {
     return <Profile state={props.state.forPosts} />;
   };
   return (
-    <BrowserRouter>
-      <div className='app_wrapper'>
-        <div className='app_wrapper_header'>
-          <Header />
-        </div>
-        <div className='app_wrapper_navbar'>
-          <Navbar state={props.state.forNavbar} />
-        </div>
-        <div className='app_wrapper_content'>
-          <Route path='/profile' render={profileCaller} />
-          <div className='app_wrapper_dialogs'>
-            {/* так сделано что бы в итоге не путать адрес и компоненту */}
-            <Route exact path='/messages' render={dialogsCaller} />
-          </div>
-          <Route path='/news' component={News} />
-          <Route path='/music' component={Music} />
-          <Route path='/settings' component={Settings} />
-        </div>
+    <div className='app_wrapper'>
+      <div className='app_wrapper_header'>
+        <Header />
       </div>
-    </BrowserRouter>
+      <div className='app_wrapper_navbar'>
+        <Navbar state={props.state.forNavbar} />
+      </div>
+      <div className='app_wrapper_content'>
+        <Route path='/profile' render={profileCaller} />
+        <div className='app_wrapper_dialogs'>
+          {/* так сделано что бы в итоге не путать адрес и компоненту */}
+          <Route exact path='/messages' render={dialogsCaller} />
+        </div>
+        <Route path='/news' component={News} />
+        <Route path='/music' component={Music} />
+        <Route path='/settings' component={Settings} />
+      </div>
+    </div>
   );
 };
 
