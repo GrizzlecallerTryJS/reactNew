fabf96ba74c7e55cc1f110cab347cf601a856122
GrizzlecallerTryJS <dylebedev.coding@gmail.com>
2020-09-20 22:09:40 +0300

    MT-031 done

--- src/redux/__tests__/postReducer.test.js
+++ src/redux/__tests__/postReducer.test.js
@@ -1,6 +1,4 @@
-import postReducer, { addPost, deletePost } from '../Post-Reducer';
-import React from 'react';
-import { render } from '@testing-library/react';
+import postReducer, { addPost, deletePost, likeButtonCounter } from '../Post-Reducer';
 /* test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
@@ -8,7 +6,7 @@ import { render } from '@testing-library/react';
 }); */
 
 let state = {
-  postData: [
+  postData : [
     { id: 1, message: 'BEEP', likeCount: 15, liked: false },
     { id: 2, message: 'BOOP', likeCount: 20, liked: false },
     { id: 3, message: 'BOOP', likeCount: 20, liked: false },
@@ -20,15 +18,20 @@ let state = {
 };
 
 test('length of postData should be incremented', () => {
-
   const action = addPost('beepx7');
   const newState = postReducer(state, action);
 
   expect(newState.postData.length).toBe(8);
 });
 
-test('LikeCount should be 20', () => {
+test('id new post should be incremented', () => {
+  const action = addPost('beepx7');
+  const newState = postReducer(state, action);
 
+  expect(newState.postData[7].id).toBe(8);
+});
+
+test('LikeCount should be 20', () => {
   const action = addPost('beepx7');
   const newState = postReducer(state, action);
 
@@ -36,15 +39,13 @@ test('LikeCount should be 20', () => {
 });
 
 test('message should be correct', () => {
-
   const action = addPost('beepx7');
   const newState = postReducer(state, action);
 
-  expect(newState.postData[7].message).toBe("beepx7");
+  expect(newState.postData[7].message).toBe('beepx7');
 });
 
 test('after deleting post, post massive length should be decrement', () => {
-
   const action = deletePost(1);
   const newState = postReducer(state, action);
 
@@ -52,7 +53,6 @@ test('after deleting post, post massive length should be decrement', () => {
 });
 
 test('after deleting post with postID = 1, post on position 0 should have message on postID = 2', () => {
-
   const action = deletePost(1);
   const newState = postReducer(state, action);
 
@@ -60,9 +60,22 @@ test('after deleting post with postID = 1, post on position 0 should have messag
 });
 
 test('if postId incorrect postData length should not be decrement ', () => {
-
   const action = deletePost(1000);
   const newState = postReducer(state, action);
 
   expect(newState.postData.length).toBe(7);
+});
+
+test('if like button pushed on post id = 5, likeCounter should be increase', () => {
+  const action = likeButtonCounter(5);
+  const newState = postReducer(state, action);
+
+  expect(newState.postData[4].likeCount).toBe(21);
+});
+
+test('if like button pushed on post id = 6, liked state should be true', () => {
+  const action = likeButtonCounter(6);
+  const newState = postReducer(state, action);
+
+  expect(newState.postData[5].liked).toBe(true);
 });
