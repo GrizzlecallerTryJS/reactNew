eee00b89d4f5ddea6d2160cfafd42a42699c39d6
GrizzlecallerTryJS <dylebedev.coding@gmail.com>
2020-11-24 17:32:53 +0300

    Updates

---
+++ precache-manifest.725255173d15bbe2cff3f52982f1eb24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78e607e40bc5edfe0fde6385675bea44",
+    "url": "/reactNew/index.html"
+  },
+  {
+    "revision": "007235cbb28887c3c5c7",
+    "url": "/reactNew/static/css/main.4283ac90.chunk.css"
+  },
+  {
+    "revision": "df53987eb1c6476186dd",
+    "url": "/reactNew/static/js/2.ec7c0b16.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/reactNew/static/js/2.ec7c0b16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "007235cbb28887c3c5c7",
+    "url": "/reactNew/static/js/main.07961ca3.chunk.js"
+  },
+  {
+    "revision": "a8e1fb9dd8f45c547f80",
+    "url": "/reactNew/static/js/runtime-main.2612b46d.js"
+  },
+  {
+    "revision": "90a8ad7757ebe9dda0511e6c32aba764",
+    "url": "/reactNew/static/media/defaultImage.90a8ad77.jpg"
+  }
+]);
