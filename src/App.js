11b6d827e4c0c3f2d6e2173237a11d8ac263da69
GrizzlecallerTryJS <dylebedev.coding@gmail.com>
2020-12-17 16:45:58 +0300

    deploy_1

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import React from "react";
-import "./App.css";
-import News from "./components/News/News";
-import Music from "./components/Music/Music";
-import Settings from "./components/Settings/Settings";
-import Login from "./components/Login/Login";
-import { Route, withRouter } from "react-router-dom";
-import Friends from "./components/Friends/Friends";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
-import NavbarContainer from "./components/Navbar/NavbarContainer";
-import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
-import HeaderContainer from "./components/Header/HeaderContainer";
-import { connect } from "react-redux";
-import { initializeApp } from "./redux/App-Reducer";
-import { compose } from "redux";
-import Preloader from "./assets/loaders/Preloader/Preloader";
-import store from "./redux/redux-store";
-import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import './App.css';
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
+import Login from './components/Login/Login';
+import { Route, withRouter } from 'react-router-dom';
+import Friends from './components/Friends/Friends';
+import DialogsContainer from './components/Dialogs/DialogsContainer';
+import NavbarContainer from './components/Navbar/NavbarContainer';
+import UsersContainer from './components/Users/UsersContainer';
+import ProfileContainer from './components/Profile/ProfileContainer';
+import HeaderContainer from './components/Header/HeaderContainer';
+import { connect } from 'react-redux';
+import { initializeApp } from './redux/App-Reducer';
+import { compose } from 'redux';
+import Preloader from './assets/loaders/Preloader/Preloader';
+import store from './redux/redux-store';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends React.Component {
-  componentDidMount() {
+  componentDidMount () {
     this.props.initializeApp();
   }
-  render() {
+  render () {
     const DialogsCaller = () => {
       return <DialogsContainer />;
     };
@@ -48,24 +48,24 @@ class App extends React.Component {
       return <Preloader />;
     }
     return (
-      <div className="app_wrapper">
-        <div className="app_wrapper_header">
+      <div className='app_wrapper'>
+        <div className='app_wrapper_header'>
           <HeaderCaller />
         </div>
-        <div className="app_wrapper_navbar">
+        <div className='app_wrapper_navbar'>
           <NavbarCaller />
         </div>
-        <div className="app_wrapper_content">
-          <Route path="/profile/:userId?" component={ProfileCaller} />
-          <div className="app_wrapper_dialogs">
-            <Route exact path="/messages" component={DialogsCaller} />
+        <div className='app_wrapper_content'>
+          <Route path='/profile/:userId?' component={ProfileCaller} />
+          <div className='app_wrapper_dialogs'>
+            <Route exact path='/messages' component={DialogsCaller} />
           </div>
-          <Route path="/news" component={News} />
-          <Route path="/music" component={Music} />
-          <Route path="/settings" component={Settings} />
-          <Route path="/friends" component={Friends} />
-          <Route path="/users" component={UsersCaller} />
-          <Route path="/login" component={Login} />
+          <Route path='/news' component={News} />
+          <Route path='/music' component={Music} />
+          <Route path='/settings' component={Settings} />
+          <Route path='/friends' component={Friends} />
+          <Route path='/users' component={UsersCaller} />
+          <Route path='/login' component={Login} />
         </div>
       </div>
     );
@@ -80,20 +80,17 @@ class App extends React.Component {
 
 const mapStateToProps = (state) => {
   return {
-    initialized: state.forApp.initialized,
+    initialized : state.forApp.initialized,
   };
 };
 
 //export default compose(withRouter, connect(mapStateToProps, { initializeApp }))(App);
 
-const AppContainer = compose(
-  withRouter,
-  connect(mapStateToProps, { initializeApp })
-)(App);
+const AppContainer = compose(withRouter, connect(mapStateToProps, { initializeApp }))(App);
 
 const MainApp = (props) => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Provider store={store}>
         <AppContainer />
       </Provider>
